67303e84ff3a2ff8a2688483cf9d1a28fa124050
Davi Honorato <daviluizjk0@gmail.com>
2026-02-25 21:30:54 -0300

    Update GTM snippet in head

--- app/layout.tsx
+++ app/layout.tsx
@@ -155,11 +155,13 @@ export default function RootLayout({
         <RetargetingPixels />
 
         {/* Google Tag Manager */}
-        <Script
-          id="gtm-inline"
-          strategy="afterInteractive"
+        <script
           dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':\nnew Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],\nj=d.createElement(s),dl=l!='dataLayer'? '&l='+l: '';j.async=true;j.src=\n'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);\n})(window,document,'script','dataLayer','GTM-5NJ3XP3G');`,
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5NJ3XP3G');`,
           }}
         />
 
@@ -284,14 +286,8 @@ export default function RootLayout({
       </head>
       <body className="font-sans antialiased">
         {/* Google Tag Manager (noscript) */}
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-5NJ3XP3G"
-            height="0"
-            width="0"
-            style={{ display: 'none', visibility: 'hidden' }}
-          ></iframe>
-        </noscript>
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5NJ3XP3G"
+        height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe></noscript>
         <ErrorBoundary>
           {children}
           {/* FloatingReviews removido - poluio visual */}
